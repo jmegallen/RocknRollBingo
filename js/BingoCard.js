d1db78885b41a08d147c9920d0993267bc7aabe8
Jamie Gallen <jme@gmail.com>
2017-08-08 16:55:40 +0200

    FreePlay - 1

--- js/BingoCard.js
+++ js/BingoCard.js
@@ -1,22 +1,29 @@
-$(function()
-{
-    $("#bingoGen").on("click", function()
-    {
-        //retrieve values from textbox
-        var bdlst = $('#bands').val().split(/\n/);
+// $(function()
+// {
+//     $("#bingoGen").on("click", function()
+//     {
+//         //retrieve values from textbox
+//         var bdlst = $('#bands').val().split(/\n/);
 
-        //create an array   
-        var texts = [];
-        for (var i=0; i < bdlst.length; i++) {
-            // only push this line if it contains a non whitespace character.
-            if (/\S/.test(bdlst[i])) {
-                texts.push($.trim(bdlst[i]).split(","));
-            }
-            if (/^[,]+$/.test(bdlst[i][i])) {
-                texts.push($.trim(bdlst[i][i]));
-            }
-        }
+//         //create an array   
+//         var texts = [];
+//         for (var i=0; i < bdlst.length; i++) {
+//             // only push this line if it contains a non whitespace character.
+//             if (/\S/.test(bdlst[i])) {
+//                 texts.push($.trim(bdlst[i]).split(","));
+//             }
+//             if (/^[,]+$/.test(bdlst[i][i])) {
+//                 texts.push($.trim(bdlst[i][i]));
+//             }
+//         }
 
-        console.log(texts);
-    });
-});
+//         console.log(texts);
+//     });
+// });
+'use strict';
+
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello, world!'
+), document.getElementById('root'));
